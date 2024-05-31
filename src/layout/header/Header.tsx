aa4514d892c1b25b8ceb8191703cecc3a0be7d49
Maryie <mashenka.zinkevich0245@gmail.com>
2024-05-31 17:15:35 +0300

    code updated one

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import { Logo } from "../../components/logo/logo";
-import { Menu } from "../../components/menu/menu";
 
 export const Header = () => {
     return (
-        <header>
-            
-            <Logo/>
-            <Menu/>
-        </header>
+        <StyledHeader>
+
+            {/*<Logo/>
+            <Menu/>*/}
+        </StyledHeader>
     );
 };
 
+const StyledHeader = styled.header`
+background-color: lightgreen;
+`
