68f6bf7efe98a2ca6e4c12edfbd583602ee02d6e
Maryie <mashenka.zinkevich0245@gmail.com>
2024-10-14 18:40:53 +0300

    update

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+import { Logo } from "../../components/logo/Logo";
+import { Menu } from "../../components/menu/Menu";
 
 export const Header = () => {
     return (
         <StyledHeader>
 
-            {/*<Logo/>
-            <Menu/>*/}
+            <Logo/>
+            <Menu/>
+            
         </StyledHeader>
     );
 };
